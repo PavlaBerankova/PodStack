6333c4a0b575813ab0e6aae0c22d558b068c7df2
Pavla_Beránková <berankovap@hotmail.com>
2024-11-28 14:59:28 +0100

    Initial Commit

---
+++ PodcastExt/PodcastExtApp.swift
@@ -0,0 +1,17 @@
+//
+//  PodcastExtApp.swift
+//  PodcastExt
+//
+//  Created by Pavla Beránková on 28.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct PodcastExtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
